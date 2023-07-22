5f53dc0d806c9f59b46f34a6fa7a9d6164ff51fc
Leonardo Rincon <jleobotache@gmail.com>
2023-07-22 09:50:14 -0500

    Porject init

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,50 @@
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
+
+import logo from '../logo.svg';
+
+export const Navigation = () => {
+ return (
+  <BrowserRouter>
+   <div className='main-layout'>
+    <nav>
+     <img src={logo} alt='React Logo' />
+     <ul>
+      <li>
+       <NavLink
+        to='/lazy1'
+        className={({ isActive }) => (isActive ? 'nav-active' : '')}
+       >
+        Home
+       </NavLink>
+      </li>
+      <li>
+       <NavLink
+        to='/lazy2'
+        className={({ isActive }) => (isActive ? 'nav-active' : '')}
+       >
+        About
+       </NavLink>
+      </li>
+      <li>
+       <NavLink
+        to='/lazy3'
+        className={({ isActive }) => (isActive ? 'nav-active' : '')}
+       >
+        Users
+       </NavLink>
+      </li>
+     </ul>
+    </nav>
+
+    <Routes>
+     <Route path='/lazy1' element={<h1>About Page</h1>} />
+     <Route path='/lazy2' element={<h1>Users Page</h1>} />
+     <Route path='/lazy3' element={<h1>Home Page</h1>} />
+
+     <Route path='/*' element={<Navigate to='/lazy1' replace />} />
+    </Routes>
+   </div>
+  </BrowserRouter>
+ );
+};
